8f2ee25b2bea2bcba959c599f62ccd079e5e7d19
Alisha <alisha.kawaguchi@usenucleus.cloud>
2022-12-30 11:05:31 -0800

    test

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as exec from '@actions/exec'
 export async function run(): Promise<{environment?: string}> {
   try {
     const environment: string = core.getInput('environment', {required: true})
-    await exec.exec('nucleus', ['deploy', '-env', environment])
+    await exec.exec('nucleus', ['deploy', '--env', environment])
     return {environment}
   } catch (error) {
     const errMsg =
